be19c8503ed024e083934f3dcdf1889020022d29
theordias <theo.dias@cresta.ai>
2024-01-12 09:48:43 -0800

    Change colorscheme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,41 +1,8 @@
-require('rose-pine').setup({
-    variant = 'auto',
-	--- @usage 'main'|'moon'|'dawn'
-	dark_variant = 'main',
-	--- @usage string hex value or named color from rosepinetheme.com/palette
-	groups = {
-		background = 'base',
-		background_nc = '_experimental_nc',
-		panel = 'surface',
-		panel_nc = 'base',
-		border = 'highlight_med',
-		comment = 'muted',
-		link = 'iris',
-		punctuation = 'subtle',
-
-		error = 'love',
-		hint = 'iris',
-		info = 'foam',
-		warn = 'gold',
-
-		headings = {
-			h1 = 'iris',
-			h2 = 'foam',
-			h3 = 'rose',
-			h4 = 'gold',
-			h5 = 'pine',
-			h6 = 'foam',
-		}
-	}
+require('kanagawa').setup({
+    background = { -- map the value of 'background' option to a theme
+        dark = "wave",
+        light = "wave"
+    }
 })
 
-function ColorMyPencils(color)
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
-
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
-end
-
-ColorMyPencils()
+vim.cmd("colorscheme kanagawa")
